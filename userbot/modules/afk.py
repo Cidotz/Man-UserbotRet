c385fa43dd2301d6130b6431ef83a9f4adc8bfa0
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-12 15:23:46 +0700

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -27,67 +27,55 @@ from userbot import (  # noqa pylint: disable=unused-import isort:skip
 global USER_AFK
 global afk_time
 global last_afk_message
-global last_afk_msg
 global afk_start
 global afk_end
 USER_AFK = {}
 afk_time = None
 last_afk_message = {}
-last_afk_msg = {}
 afk_start = {}
 
 
 @bot.on(events.NewMessage(outgoing=True))
-@bot.on(events.MessageEdited(outgoing=True))
 async def set_not_afk(event):
+    if event.fwd_from:
+        return
     global USER_AFK
     global afk_time
     global last_afk_message
     global afk_start
     global afk_end
-    back_alive = datetime.now()
-    afk_end = back_alive.replace(microsecond=0)
+    came_back = datetime.datetime.now()
+    afk_end = came_back.replace(microsecond=0)
     if afk_start != {}:
         total_afk_time = str((afk_end - afk_start))
     current_message = event.message.message
-    if "afk" not in current_message and "yes" in USER_AFK:
+    if "#" not in current_message and "yes" in USER_AFK:
+        xx = await event.client.send_message(
+            event.chat_id,
+            f"**{ALIVE_NAME} Pengangguran So Sibuk Balik Lagi!**\n**Dari AFK :** `{total_afk_time}` **Yang Lalu**",
+            file=pic,
+        )
         try:
-            if pic.endswith((".tgs", ".webp")):
-                shite = await bot.send_message(event.chat_id, file=pic)
-                shites = await bot.send_message(
-                    event.chat_id,
-                    f"**{ALIVE_NAME} Kembali Online Untuk Parming**\n**Dari AFK :** `{total_afk_time}` **Yang Lalu**",
-                )
-            else:
-                shite = await bot.send_message(
-                    event.chat_id,
-                    f"**{ALIVE_NAME} Pengangguran sok Sibuk Balik Lagi!**\n**Dari AFK :** `{total_afk_time}` **Yang Lalu**",
-                    file=pic,
-                )
-        except BaseException:
-            shite = await bot.send_message(
-                event.chat_id,
-                f"**{ALIVE_NAME} Kembali Online**\n**Dari AFK :** `{total_afk_time}` **Yang Lalu**",
+            await unsave_gif(event, xx)
+        except:
+            pass
+        try:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"**#AFK Mode** = `False`\n{ALIVE_NAME} Pengangguran So Sibuk Balik Lagi!**\nDari AFK :** `{total_afk_time}` **Yang Lalu**"
             )
-
-        await asyncio.sleep(6)
-        await shite.delete()
-        try:
-            await shites.delete()
         except BaseException:
             pass
+        await asyncio.sleep(5)
+        await xx.delete()
         USER_AFK = {}
         afk_time = None
 
-        os.system("rm -rf *.webp")
-        os.system("rm -rf *.mp4")
-        os.system("rm -rf *.tgs")
-        os.system("rm -rf *.png")
-        os.system("rm -rf *.jpg")
-
 
 @bot.on(
-    events.NewMessage(incoming=True, func=lambda e: bool(e.mentioned or e.is_private))
+    events.NewMessage(
+        incoming=True, func=lambda e: bool(e.mentioned or e.is_private)
+    )
 )
 async def on_afk(event):
     if event.fwd_from:
@@ -97,51 +85,38 @@ async def on_afk(event):
     global last_afk_message
     global afk_start
     global afk_end
-    back_alivee = datetime.now()
-    afk_end = back_alivee.replace(microsecond=0)
+    cum_back = datetime.datetime.now()
+    afk_end = cum_back.replace(microsecond=0)
     if afk_start != {}:
         total_afk_time = str((afk_end - afk_start))
     current_message_text = event.message.message.lower()
     if "afk" in current_message_text:
+        # userbot's should not reply to other userbot's
+        # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
         return False
     if USER_AFK and not (await event.get_sender()).bot:
         msg = None
-        if reason:
+        if reason == "":
+            message_to_reply = f"**✘ Maaf {ALIVE_NAME} Sedang AFK** `{total_afk_time}` **Yang Lalu ✘**"
+        else:
             message_to_reply = (
                 f"**✘ {ALIVE_NAME} Sedang AFK** `{total_afk_time}` **Yang Lalu ✘**\n"
                 + f"**✦҈͜͡➳ Karena :** `{reason}`"
-            )
-        else:
-            message_to_reply = (
-                f"**✘ Maaf {ALIVE_NAME} Sedang AFK** `{total_afk_time}` **Yang Lalu ✘**"
-            )
+                )
+        msg = await event.reply(message_to_reply, file=pic)
         try:
-            if pic.endswith((".tgs", ".webp")):
-                msg = await event.reply(file=pic)
-                msgs = await event.reply(message_to_reply)
-            else:
-                msg = await event.reply(message_to_reply, file=pic)
-        except BaseException:
-            msg = await event.reply(message_to_reply)
-        await asyncio.sleep(2.5)
+            await unsave_gif(event, msg)
+        except:
+            pass
+        await asyncio.sleep(2)
         if event.chat_id in last_afk_message:
             await last_afk_message[event.chat_id].delete()
-        try:
-            if event.chat_id in last_afk_msg:
-                await last_afk_msg[event.chat_id].delete()
-        except BaseException:
-            pass
         last_afk_message[event.chat_id] = msg
-        try:
-            if msgs:
-                last_afk_msg[event.chat_id] = msgs
-        except BaseException:
-            pass
 
 
 @register(
-    outgoing=True, pattern=r"^\.afk(?: |$)(.*)", disable_errors=True
-)  # pylint:disable=E0602
+    outgoing=True, pattern=r"^\.afk ?(.*)", disable_errors=True
+)
 async def _(event):
     if event.fwd_from:
         return
@@ -149,7 +124,6 @@ async def _(event):
     global USER_AFK
     global afk_time
     global last_afk_message
-    global last_afk_msg
     global afk_start
     global afk_end
     global reason
@@ -157,91 +131,60 @@ async def _(event):
     USER_AFK = {}
     afk_time = None
     last_afk_message = {}
-    last_afk_msg = {}
     afk_end = {}
-    start_1 = datetime.now()
+    start_1 = datetime.datetime.now()
     afk_start = start_1.replace(microsecond=0)
-    reason = event.pattern_match.group(1)
-    pic = await event.client.download_media(reply) if reply else None
+    owo = event.text[5:]
+    reason = owo
+    pic = await event.client.download_media(reply)
     if not USER_AFK:
         last_seen_status = await bot(
             functions.account.GetPrivacyRequest(types.InputPrivacyKeyStatusTimestamp())
         )
         if isinstance(last_seen_status.rules, types.PrivacyValueAllowAll):
             afk_time = datetime.datetime.now()
-        USER_AFK = f"yes: {reason} {pic}"
-        if reason:
+        if owo == "":
+            USER_AFK = f"yes: not-mentiond {pic}"
+            x = await bot.send_message(
+                event.chat_id, f"**Bye Gua AFK Dulu**", file=pic)
             try:
-                if pic.endswith((".tgs", ".webp")):
-                    await bot.send_message(event.chat_id, file=pic)
-                    await bot.send_message(
-                        event.chat_id,
-                        f"\n**✘ {ALIVE_NAME} Telah AFK ✘**\n**✦҈͜͡➳ Karena :** `{reason}`",
-                    )
-                else:
-                    await bot.send_message(
-                        event.chat_id,
-                        f"\n**✘ {ALIVE_NAME} Telah AFK ✘**\n**✦҈͜͡➳ Karena :** `{reason}`",
-                        file=pic,
-                    )
-            except BaseException:
-                await bot.send_message(
-                    event.chat_id,
-                    f"\n**✘ {ALIVE_NAME} Telah AFK ✘**\n**✦҈͜͡➳ Karena :** `{reason}`",
-                )
-        else:
+                await unsave_gif(event, x)
+            except:
+                pass
+            await asyncio.sleep(0.001)
+            await event.delete()
             try:
-                if pic.endswith((".tgs", ".webp")):
-                    await bot.send_message(event.chat_id, file=pic)
-                    await bot.send_message(
-                        event.chat_id, f"**✘ {ALIVE_NAME} Telah AFK ✘**"
-                    )
-                else:
-                    await bot.send_message(
-                        event.chat_id,
-                        f"**✘ {ALIVE_NAME} Telah AFK ✘**",
-                        file=pic,
-                    )
-            except BaseException:
-                await bot.send_message(event.chat_id, f"**✘ {ALIVE_NAME} Telah AFK ✘**")
-        await event.delete()
-        try:
-            if reason and pic:
-                if pic.endswith((".tgs", ".webp")):
-                    await bot.send_message(BOTLOG_CHATID, file=pic)
-                    await bot.send_message(
-                        BOTLOG_CHATID,
-                        f"\n**✘ {ALIVE_NAME} Sedang AFK ✘**\n**✦҈͜͡➳ Karena :** `{reason}`",
-                    )
-                else:
-                    await bot.send_message(
-                        BOTLOG_CHATID,
-                        f"\n**✘ {ALIVE_NAME} Sedang AFK ✘**\n**✦҈͜͡➳ Karena :** `{reason}`",
-                        file=pic,
-                    )
-            elif reason:
-                await bot.send_message(
+                xy = await bot.send_message(
                     BOTLOG_CHATID,
-                    f"\n**✘ {ALIVE_NAME} Sedang AFK ✘**\n**✦҈͜͡➳ Karena :** `{reason}`",
-                )
-            elif pic:
-                if pic.endswith((".tgs", ".webp")):
-                    await bot.send_message(BOTLOG_CHATID, file=pic)
-                    await bot.send_message(
-                        BOTLOG_CHATID, f"\n**✘ {ALIVE_NAME} Sedang AFK ✘**"
+                    f"**#AFK Mode** = `True`",file=pic
                     )
-                else:
-                    await bot.send_message(
-                        BOTLOG_CHATID,
-                        f"\n**✘ {ALIVE_NAME} Sedang AFK ✘**",
-                        file=pic,
+                try:
+                    await unsave_gif(event, xy)
+                except:
+                    pass
+            except Exception as e:
+                logger.warn(str(e))
+        else:
+            USER_AFK = f"yes: {reason} {pic}"
+            x = await bot.send_message(
+                event.chat_id, f"**Bye Gua AFK Dulu**\n\n**Karena :** `{reason}`", file=pic)
+            try:
+                await unsave_gif(event, x)
+            except:
+                pass
+            await asyncio.sleep(0.001)
+            await event.delete()
+            try:
+                xy = await bot.send_message(
+                    BOTLOG_CHATID,
+                    f"**#AFK Mode** = `True`\n**Karena:** `{reason}`",file=pic
                     )
-            else:
-                await bot.send_message(
-                    BOTLOG_CHATID, f"\n**✘ {ALIVE_NAME} Sedang AFK ✘**"
-                )
-        except Exception as e:
-            BOTLOG_CHATIDger.warn(str(e))
+                try:
+                    await unsave_gif(event, xy)
+                except:
+                    pass
+            except Exception as e:
+                logger.warn(str(e))
 
 
 CMD_HELP.update(
