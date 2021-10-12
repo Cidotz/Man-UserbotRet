20a683344413a7608c19578125c9a65c9a5ac9eb
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-12 19:36:46 +0700

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -198,11 +198,11 @@ async def _(dyno):
             "**Wajib Mengisi Var** `HEROKU_APP_NAME` **dan** `HEROKU_API_KEY`"
         )
     await dyno.edit("**Sedang Mengambil Logs Heroku**")
-    with open("logs.txt", "w") as log:
+    with open("Logs.txt", "w") as log:
         log.write(app.get_log())
     await dyno.client.send_file(
         entity=dyno.chat_id,
-        file="logs.txt",
+        file="Logs.txt",
         thumb="userbot/resources/logo.jpg",
         caption="**Ini Logs Heroku anda**",
     )
