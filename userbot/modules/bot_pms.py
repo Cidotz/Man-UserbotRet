fe843de6e31516e3f31811badba71c0f6e98f1c2
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-14 06:16:27 +0700

    Update bot_pms.py

--- userbot/modules/bot_pms.py
+++ userbot/modules/bot_pms.py
@@ -3,14 +3,17 @@
 # FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
 # t.me/SharingUserbot & t.me/Lunatic0de
 
+import asyncio
 import re
 from collections import defaultdict
 from datetime import datetime
 from typing import Optional, Union
 
 from telethon import Button, events
-from telethon.errors import UserIsBlockedError
 from telethon.events import CallbackQuery, StopPropagation
+from telethon.errors import UserNotParticipantError, UserIsBlockedError
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator, InputWebDocument
+from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.utils import get_display_name
 
 from userbot import (
@@ -44,6 +47,7 @@ from userbot.utils.logger import logging
 LOGS = logging.getLogger(__name__)
 
 botusername = BOT_USERNAME
+spam_chats = []
 
 
 class FloodConfig:
@@ -75,6 +79,81 @@ async def check_bot_started_users(user, event):
         LOGS.error(str(e))
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, notification)
+
+@tgbot.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
+async def mentionall(event):
+  chat_id = event.chat_id
+  if event.is_private:
+    return await event.respond("__Perintah ini hanya dapat digunakan dalam grup dan channel!__")
+
+  is_admin = False
+  try:
+    partici_ = await client(GetParticipantRequest(
+      event.chat_id,
+      event.sender_id
+    ))
+  except UserNotParticipantError:
+    is_admin = False
+  else:
+    if (
+      isinstance(
+        partici_.participant,
+        (
+          ChannelParticipantAdmin,
+          ChannelParticipantCreator
+        )
+      )
+    ):
+      is_admin = True
+  if not is_admin:
+    return await event.respond("__Hanya admin yang bisa mention semua!__")
+
+  if event.pattern_match.group(1) and event.is_reply:
+    return await event.respond("__Beri saya satu argumen!__")
+  elif event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.is_reply:
+    mode = "text_on_reply"
+    msg = await event.get_reply_message()
+    if msg == None:
+        return await event.respond("__Saya tidak bisa menyebut anggota untuk pesan lama! (pesan yang dikirim sebelum saya ditambahkan ke grup)__")
+  else:
+    return await event.respond("__Membalas pesan atau memberi saya beberapa teks untuk menyebutkan orang lain!__")
+
+  spam_chats.append(chat_id)
+  usrnum = 0
+  usrtxt = ''
+  async for usr in client.iter_participants(chat_id):
+    if not chat_id in spam_chats:
+      break
+    usrnum += 1
+    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+    if usrnum == 5:
+      if mode == "text_on_cmd":
+        txt = f"{usrtxt}\n\n{msg}"
+        await client.send_message(chat_id, txt)
+      elif mode == "text_on_reply":
+        await msg.reply(usrtxt)
+      await asyncio.sleep(2)
+      usrnum = 0
+      usrtxt = ''
+  try:
+    spam_chats.remove(chat_id)
+  except:
+    pass
+
+
+@tgbot.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+  if not event.chat_id in spam_chats:
+    return await event.respond('__Tidak ada proses yang berjalan...__')
+  else:
+    try:
+      spam_chats.remove(event.chat_id)
+    except:
+      pass
+    return await event.respond('__Stopped.__')
 
 
 @tgbot.on(
